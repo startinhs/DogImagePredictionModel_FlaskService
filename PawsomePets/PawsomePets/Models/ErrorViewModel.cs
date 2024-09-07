f0c72e13fb2def6fa0d00e42a3d23efff08653f9
Lê Minh Tính <133968641+startinhs@users.noreply.github.com>
2024-09-08 02:01:24 +0700

    init Project

---
+++ PawsomePets/PawsomePets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PawsomePets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
